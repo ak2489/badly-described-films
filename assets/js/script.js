8767a2cea52b5e3346193361140b52216a1c8d1c
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2021-09-09 20:21:50 +0000

    add js to start game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
+// Variables for quiz
 
+const startButton = document.getElementById('start-btn')
+const tag = document.getElementById('tag')
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
+// Functions for quiz
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    tag.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
